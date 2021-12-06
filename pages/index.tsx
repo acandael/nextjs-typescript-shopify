1845db6270cc0caffd740cc64ff9ba2ed23325ca
Anthony Candaele <anthony.candaele@tothepoint.company>
2021-12-06 12:31:46 +0100

    issue with normalizeProduct

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import getAllProducts from '../framework/shopify/product/get-all-products';
+
+export async function getStaticProps() {
+  const products = await getAllProducts();
+
+  return {
+    props: {
+      products,
+      revalidate: 4 * 60 * 60,
+    },
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<GetStaticProps>) {
+  return <div>{JSON.stringify(products)}</div>;
+}
